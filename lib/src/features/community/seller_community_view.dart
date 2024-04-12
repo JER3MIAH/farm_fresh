0ac95b59343a3ed23f96a4d4b2ec9d70d1e1d041
JER3MIAH <jeremiahbrownie@gmail.com>
2024-04-12 14:54:49 +0100

    Implement community view

--- lib/src/features/community/seller_community_view.dart
+++ lib/src/features/community/seller_community_view.dart
@@ -7,9 +7,38 @@ class SellerCommunityView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppColumn(
-        margin: EdgeInsets.symmetric(horizontal: 15.dx).copyWith(top: 55.dy),
-        children: const [],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.dx).copyWith(top: 55.dy),
+        child: Column(
+          children: [
+            const BackAndAppTitle(text: 'Community'),
+            YBox(93.dy),
+            Container(
+              height: 199.dy,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: appColors.grey.withOpacity(.5),
+              ),
+            ),
+            YBox(10.dy),
+            SizedBox(
+              width: Dims.dxPercent(.7),
+              child: Text(
+                'Make connections with other farmers, exchange ideas, and keep up with the newest developments in sustainable farming.',
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            YBox(100.dy),
+            AppButton(
+              title: 'Join community',
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
